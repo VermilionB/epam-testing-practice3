4580db9552c3517b71c0d3f05115624e2726fa9e
VermilionB <serezha.mostyko@mail.ru>
2023-01-05 14:36:53 +0300

    calc done

--- lab9/objects/page.js
+++
@@ -1,9 +0,0 @@
-export default class Page {
-    constructor() {
-        this.title = 'Mercedes-mallisto: uusien autojen valikoima | Mercedes-Benz';
-    }
-
-    async open(path) {
-        await browser.url(path);
-    }
-}
